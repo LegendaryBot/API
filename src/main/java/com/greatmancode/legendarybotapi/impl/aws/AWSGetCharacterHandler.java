cb8602fa906b364478b7d10ae4c1e6d7e4d267f8
William Lebel <lebelwilliam@gmail.com>
2018-04-11 22:24:24 -0400

    URL decode the value of character.

--- src/main/java/com/greatmancode/legendarybotapi/impl/aws/AWSGetCharacterHandler.java
+++ src/main/java/com/greatmancode/legendarybotapi/impl/aws/AWSGetCharacterHandler.java
@@ -6,6 +6,8 @@ import com.greatmancode.legendarybotapi.characters.CharacterHelper;
 import com.serverless.ApiGatewayResponse;
 import org.json.JSONObject;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
 import java.util.Map;
 
 public class AWSGetCharacterHandler implements RequestHandler<Map<String, Object>, ApiGatewayResponse> {
@@ -14,9 +16,19 @@ public class AWSGetCharacterHandler implements RequestHandler<Map<String, Object
         Map<String,String> pathParameters = (Map<String,String>)input.get("pathParameters");
         String serverName = pathParameters.get("realm");
         String region = pathParameters.get("region");
-        String character = pathParameters.get("character");
+        String character = null;
+        try {
+            character = URLDecoder.decode(pathParameters.get("character"), "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
 
         JSONObject result = CharacterHelper.getCharacterStatsEmbed(region, serverName, character);
+        if (result == null) {
+            return ApiGatewayResponse.builder()
+                    .setStatusCode(404)
+                    .build();
+        }
         return ApiGatewayResponse.builder()
                 .setStatusCode(result != null ? 200 : 404)
                 .setRawBody(result.toString())
