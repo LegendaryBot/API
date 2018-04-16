de36211a86e7a4295f1ded31ec2a9d14d25f4dcc
William Lebel <lebelwilliam@gmail.com>
2018-04-16 00:33:29 -0400

    Use the proper variable to get the post data.

--- src/main/java/com/greatmancode/legendarybotapi/impl/aws/AWSSetDiscordGuildSetting.java
+++ src/main/java/com/greatmancode/legendarybotapi/impl/aws/AWSSetDiscordGuildSetting.java
@@ -10,11 +10,10 @@ import java.util.Map;
 public class AWSSetDiscordGuildSetting implements RequestHandler<Map<String, Object>, ApiGatewayResponse> {
     @Override
     public ApiGatewayResponse handleRequest(Map<String, Object> input, Context context) {
-        System.out.println(input);
         Map<String,String> pathParameters = (Map<String,String>)input.get("pathParameters");
         long guildId = Long.parseLong(pathParameters.get("guildId"));
         String key = pathParameters.get("key");
-        DiscordGuildHelper.setSetting(DiscordGuildHelper.getDiscordGuild(guildId),key, input.get("post"));
+        DiscordGuildHelper.setSetting(DiscordGuildHelper.getDiscordGuild(guildId),key, input.get("body"));
         return ApiGatewayResponse.builder()
                 .setStatusCode(200)
                 .build();
